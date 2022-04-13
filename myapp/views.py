898f5dff4d9402efee702793289b737f048df660
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-04-13 13:11:29 +0500

    set the static files in setting.py and load static files,, and extent the base file in other html files ..

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def add_show(request):
+    return render(request, "addandshow.html")
+
+def base(request):
+    return render(request, "base.html")

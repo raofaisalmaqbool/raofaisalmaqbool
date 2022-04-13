e6bf173bcb2c1ffe7f677166f3a94957f47c57b5
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-04-13 13:46:06 +0500

    create migration in models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
+import email
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=20)
